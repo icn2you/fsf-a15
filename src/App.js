3f2363b4fac220579c22764242185c19f67d28d4
Christopher Zenner <christopher.zenner@gmail.com>
2020-06-25 20:34:17 -0700

    feat: add primary react app component with styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Pokemon from './components/Pokemon';
+import pokemon from './data/pokemon.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {pokemon};
+  }
+
+  render() {
+    const pokemonImgs = this.state.pokemon.map(pokemon => 
+      <Pokemon
+        id={pokemon.id}
+        key={pokemon.id}
+        name={pokemon.name}
+        img={pokemon.image}
+        clicked={false}
+      />
+    );
+
+    return (
+      <>
+        <Header />
+        <Main>{pokemonImgs}</Main>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
