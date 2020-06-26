bd575a7a2b07c96db85717740ae3d32954f7dfab
Christopher Zenner <christopher.zenner@gmail.com>
2020-06-26 13:33:45 -0700

    feat: pokémon clicked state set true on user click
    The `clicked` property of Pokémon objects is set to `true` when user
    clicks the Pokémon's respective image. If the Pokémon image has previously been clicked, an appropriate message is logged to the console. This will be replaced with a "Game over!" notification.

--- src/App.js
+++ src/App.js
@@ -9,17 +9,95 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {pokemon};
+
+    const pokeObjArr = pokemon.map(pokemon => 
+      Object.assign(pokemon, { clicked: false }));
+
+    // DEBUG:
+    console.log(`pokeObjArr = ${JSON.stringify(pokeObjArr)}`);
+
+    this.state = {pokeObjArr};
+  }
+
+  /* 
+     Thanks to Ilya Kantor at [The Modern JavaScript Tutorial][ref] for this most elegant shuffle method!
+
+     [ref](https://javascript.info/task/shuffle)
+  */
+  shuffle = (arr) => {
+    // Create a copy of the array parameter for immutability!
+    let shuffledArr = arr;
+
+    for (let i = shuffledArr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+
+      [shuffledArr[i], shuffledArr[j]] = 
+        [shuffledArr[j], shuffledArr[i]];
+
+      console.log(
+        `Shuffled array: ${JSON.stringify(arr)}`
+      );
+
+      return shuffledArr;
+    }
+  }
+
+  markClicked(id, clicked) {
+    // DEBUG:
+    console.log(`Pokemon ${id} clicked? ${clicked}`);
+    
+    if (clicked) {
+      // ASSERT: User has already clicked this Pokémon;
+      //         therefore, they lose the game.
+      console.log(
+        `Pokemon ${id} has already been clicked. You're a loser!`);
+    } else {
+      // ASSERT: User has not already clicked this Pokémon;
+      //         therefore, their score increases by five points.
+      
+      const pokeObjArr = this.shuffle(
+        this.state.pokeObjArr.map(pokemon =>
+        (pokemon.id === id) ?
+        Object.assign(pokemon, { clicked: true }) :
+        pokemon
+      ));
+
+      console.log(
+        `Test array: ${JSON.stringify(pokeObjArr)}`
+      );
+      
+      this.setState({pokeObjArr});
+
+      // DEBUG:
+      // this.setState({testArr});
+
+      // DEBUG:
+      console.log(
+        `New state: ${JSON.stringify(this.state.pokeObjArr)}`);
+
+
+
+/*       this.setState({
+        id = 
+      })
+
+      this.state.pokemon.clicked = true;
+
+      const pokemon = this.shuffle(this.state.pokemon);
+
+      this.setState({pokemon}); */
+    }
   }
 
   render() {
-    const pokemonImgs = this.state.pokemon.map(pokemon => 
+    const pokemonImgs = this.state.pokeObjArr.map(pokemon => 
       <Pokemon
         id={pokemon.id}
         key={pokemon.id}
         name={pokemon.name}
         img={pokemon.image}
-        clicked={false}
+        clicked={pokemon.clicked}
+        markClicked={this.markClicked.bind(this)}
       />
     );
 
--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 function Pokemon(props) {
-  return <img className="img-thumbnail" src={props.img} alt={props.name} />;
+  return <img id={props.id} className="img-thumbnail" src={props.img} alt={props.name} clicked={props.clicked} onClick={() => props.markClicked(props.id, props.clicked)} />;
 }
 
 export default Pokemon;
