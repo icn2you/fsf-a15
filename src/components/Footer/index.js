943dc5712face8ee16afedbccbc14bbcd5306df7
Christopher Zenner <christopher.zenner@gmail.com>
2020-06-27 12:23:46 -0700

    chore: disclaim copyright and modify attributions

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,10 +6,10 @@ function Footer() {
     <footer>
       <div className="container-fluid text-center">
         <div id="copyright">
-          <i className="fab fa-react fa-lg"></i>&nbsp;&nbsp;&nbsp;&copy; 2020 Christopher B. Zenner. All rights reserved.&nbsp;&nbsp;&nbsp;<i className="fab fa-react fa-lg"></i>
+          <i className="fab fa-react fa-lg"></i>&nbsp;&nbsp;&nbsp;Built with <i class="fas fa-heart"></i> by Christopher B. Zenner. No rights reserved.&nbsp;&nbsp;&nbsp;<i className="fab fa-react fa-lg"></i>
         </div>
         <div id="attribution">
-          Jumbotron background pattern courtesy of <a href="https://dribbble.com/shots/6019214-Daily-UI-059/attachments" target="_blank" rel="noopener noreferrer">Yvonne Cavero</a>. Fonts served by <a href="https://fonts.google.com/" target="_blank" rel="noopener noreferrer">Google Fonts</a>.
+          Jumbotron background pattern courtesy of <a href="https://dribbble.com/shots/6019214-Daily-UI-059/attachments" target="_blank" rel="noopener noreferrer">Yvonne Cavero</a>. Fonts served by <a href="https://fonts.google.com/" target="_blank" rel="noopener noreferrer">Google Fonts</a>. <br />Pokémon images, including the Pokéball favicon and Pokéball background pattern, are copyrighted by their respective creators.
         </div>
       </div>
     </footer>
