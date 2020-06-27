c0676836f455a3dce44a45ac607c20dd2e62c312
Christopher Zenner <christopher.zenner@gmail.com>
2020-06-27 11:32:03 -0700

    Updates

---
+++ precache-manifest.900d2abdbec03368994bf29e71107091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0565c40985612f1910e40464c7e3dd6f",
+    "url": "/fsf-a15/index.html"
+  },
+  {
+    "revision": "9fa1dfe7d29087bf798e",
+    "url": "/fsf-a15/static/css/main.612fbc2c.chunk.css"
+  },
+  {
+    "revision": "59e3f9e270d1f8260418",
+    "url": "/fsf-a15/static/js/2.7087a5b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fsf-a15/static/js/2.7087a5b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa1dfe7d29087bf798e",
+    "url": "/fsf-a15/static/js/main.104b2ce1.chunk.js"
+  },
+  {
+    "revision": "3bb611703fc8a4b8235a",
+    "url": "/fsf-a15/static/js/runtime-main.aaa97cd7.js"
+  },
+  {
+    "revision": "7bd1424d6c5150762d3722264d2a4a21",
+    "url": "/fsf-a15/static/media/pokeball-bg.7bd1424d.jpg"
+  }
+]);
